3659ced5aa88a68a710cf8c813de221a5d1e6751
Renato Cardoso <re2005@gmail.com>
2015-04-23 01:00:33 +0200

    Google Maps

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,48 @@
+	////////////////////////////////////////////////////////////
+	// GOOGLE MAPS
+	///////////////////////////////////////////////////////////
+
+	function initialize() {
+		var mapOptions = {
+			zoom: 10,
+			center: new google.maps.LatLng(48.8421607,2, -2.3994408,13)
+		}
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		var myLatLng = new google.maps.LatLng(48.8421607,2, -2.3994408,13);
+		var hotelMarker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: 'Your Hotel'
+		});
+
+		var myPoint1 = new google.maps.LatLng(48.7421607,2, -2.3994405,13);
+		var hotelPoint = new google.maps.Marker({
+			position: myPoint1,
+			map: map,
+			title:'Nice Museum to visit'
+		});
+
+		var myPoint2 = new google.maps.LatLng(48.9421607,2, -2.3994405,13);
+		var hotelPoint = new google.maps.Marker({
+			position: myPoint2,
+			map: map,
+			title:'Amazing restaurant'
+		});
+	}
+
+
+setTimeout(function(){ 
+	google.maps.event.addDomListener(window, 'load', initialize);
+ }, 500);
+
+
+	
+
+
+/*
+
+
 
 window.fbAsyncInit = function() {
 	FB.init({
@@ -14,3 +59,7 @@ window.fbAsyncInit = function() {
 	js.src = "//connect.facebook.net/en_US/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+
+*/
