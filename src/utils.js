74661d88bf28891e3620eff873f239c9fdcb3fdf
Renato Cardoso <re2005@gmail.com>
2015-04-23 12:46:07 +0200

    Css Updates, Products List

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,10 @@
 
 		var mapOptions = {
 			zoom: 10,
-			center: new google.maps.LatLng(48.8421607,2, -2.3994408,13)
+			center: new google.maps.LatLng(48.8421607,2, -2.3994408,13),
+			mapTypeControl: false,
+			zoomControl: false,
+			streetViewControl: false
 		}
 		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
@@ -35,7 +38,6 @@
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
-	
 
 // Set geolocation
 function getLocation() {
