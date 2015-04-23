fa807bb5c5fbbceecc25e16c89c2db4923bb0674
Renan Gonçalves <renan.saddam@gmail.com>
2015-04-23 14:47:03 +0200

    Calculating distance.

--- src/productList.jsx.js
+++ src/productList.jsx.js
@@ -9,16 +9,38 @@ var ProductList = React.createClass({
 
 	componentWillMount: function() {
 		var firebaseRef = new Firebase('https://hackbattle2015.firebaseio.com/products/');
-		this.bindAsArray(firebaseRef.limitToLast(25), 'products');
+		var products = firebaseRef.limitToLast(25);
+
+		this.bindAsArray(products, 'products');
 	},
 
-	listItem: function(item, index) {
+	calculateDistance: function(coordinates) {
+		if (typeof this.props.byDistance === 'undefined') {
+			return;
+		}
+
+		var geodata = new Geodata(coordinates);
+		var distance = geodata.distance(this.props.byDistance);
+
+		return this.convertDistance(distance);
+	},
+
+	convertDistance: function(meters) {
+		if (meters < 1000) {
+			return meters.toFixed(0) + ' meters';
+		} else {
+			var kilometers = meters / 1000;
+			return kilometers.toFixed(1) + ' km';
+		}
+	},
+
+	displayItem: function(item, index) {
 		return (
 			<li className="product">
 				<img className="image" src={item.image} />
 				<div className="title">{item.name}</div>
 				<div className="price">{item.price}</div>
-				<div className="distance">2km</div>
+				<div className="distance">{this.calculateDistance(item.geodata)}</div>
 				<div className="buy">buy</div>
 			</li>
 		);
@@ -26,7 +48,7 @@ var ProductList = React.createClass({
 
 	render: function() {
 		return (
-			<ul>{ this.state.products.map(this.listItem) }</ul>
+			<ul>{ this.state.products.map(this.displayItem) }</ul>
 		);
 	}
 });
